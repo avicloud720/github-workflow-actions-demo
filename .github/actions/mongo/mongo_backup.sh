1c0d7be70c5ce42ca91d1546204d739283a7f360
Avishek Pattanaik <avicloud720@avicloud720.local>
2022-08-12 01:37:41 +0530

    demo commit 22

--- .github/actions/mongo/mongo_backup.sh
+++ .github/actions/mongo/mongo_backup.sh
@@ -1,16 +1,15 @@
 #!/bin/sh -l
 
 echo "Connecting to MongoDB Atlas......"
-brew install mongodb-atlas
 atlas --version
 
-echo -e "\n\n============Configuration Setup===============\n\n"
-atlas config set public_api_key gripbfjg
-atlas config set private_api_key 5cc3328a-9ff1-4bbb-9e98-c5380172e1e7
+echo "\n\n============Configuration Setup===============\n\n"
+atlas config set public_api_key lwyjahcy
+atlas config set private_api_key 53156a8d-44e7-4715-9323-fae8b1fc2ad2
 atlas config set project_id 62f3427084a04c48f05f8a7e
 atlas config set org_id 62f342087fa66d7a256e53b5
 
-echo -e "\n\n============Cluster List===============\n\n"
+echo "\n\n============Cluster List===============\n\n"
 echo "This is $1"
 clusters_list=`atlas clusters list`
 echo "::set-output name=clusters_list::$clusters_list"
