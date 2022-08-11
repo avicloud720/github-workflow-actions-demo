900dc2ac9ee1fce4cb680f7b3429c01f2949d29a
Avishek Pattanaik <avicloud720@avicloud720.local>
2022-08-11 23:24:12 +0530

    demo commit 22

--- .github/actions/mongo/mongo_backup.sh
+++ .github/actions/mongo/mongo_backup.sh
@@ -1,7 +1,13 @@
 #!/bin/sh -l
 
 echo "Connecting to MongoDB Atlas......"
-brew install mongodb-atlas
+wget -qO - https://pgp.mongodb.com/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-atlas-cli
+sudo apt-get install --only-upgrade mongodb-atlas-cli
+
+# brew install mongodb-atlas
 atlas --version
 
 echo -e "\n\n============Configuration Setup===============\n\n"
