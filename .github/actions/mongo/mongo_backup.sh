c472dc57b6063c7ef276bdd83fd98a99853b305b
Avishek Pattanaik <avicloud720@avicloud720.local>
2022-08-12 09:55:25 +0530

    demo commit 22

--- .github/actions/mongo/mongo_backup.sh
+++ .github/actions/mongo/mongo_backup.sh
@@ -10,7 +10,7 @@ atlas config set project_id $MONGO_ATLAS_PROJECT_ID
 atlas config set org_id $MONGO_ATLAS_ORG_ID
 # atlas organizations apiKeys accessLists create --apiKey $MONGO_ATLAS_PUB_API_KEY
 # atlas accessList create --currentIP
-atlas accessList create '0.0.0.0/0'
+atlas accessList create $runner_ip
 
 echo "\n============Cluster List==============="
 echo "This is $1"
